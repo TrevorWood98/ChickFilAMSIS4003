0695daee51392b0943ff70485af0988db8af36e4
TrevorWood98 <100435421+TrevorWood98@users.noreply.github.com>
2022-04-11 13:28:25 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿Drop Table SubscriptionForm
+go
+
+CREATE TABLE SubscriptionForm (
+SubscriberID int IDENTITY(1,1) NOT NULL, 
+SubscriberName varchar (30) NOT NULL,
+SubscriberLast varchar (30) NOT NULL,
+SubscriberEmail varchar (50) NOT NULL)
+
+ALTER TABLE [dbo].[SubscriptionForm] ADD CONSTRAINT subscriberpk PRIMARY KEY (SubscriberID);
+
+select * from SubscriptionForm
---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE CaterRequest(
+CaterRequestID int IDENTITY(1,1) NOT NULL,
+FirstName varchar(30) NOT NULL,
+LastName varchar(30) NOT NULL,
+EmailAddress varchar(50) NOT NULL,
+EventDate date NOT NULL,
+EventTime time (0) NOT NULL, 
+GuestCount int NOT NULL,
+LocationAddress1 varchar (50) NOT NULL, 
+LocationAddress2 varchar (50) NULL, 
+City varchar (50) NOT NULL,
+RequestStateID int NOT NULL,
+ZipCode char (5) NOT NULL, 
+EventDetails varchar (100) NOT NULL);
+
+ALTER TABLE [dbo].[CaterRequest] ADD CONSTRAINT CaterRequestpk PRIMARY KEY (CaterRequestID);
+ALTER TABLE [dbo].[CaterRequest] WITH CHECK ADD CONSTRAINT CaterRequestFK FOREIGN KEY ([RequestStateID])
+REFERENCES StatesMaster ([StateID]);
+GO
+
